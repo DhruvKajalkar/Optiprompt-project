e56a41ac30afd3766bb8a65d1d7675135d92681a
DhruvKajalkar <dkajalkar07@gmail.com>
2026-02-28 01:40:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/optiprompt_db"
+DATABASE_URL = "sqlite:///./optiprompt.db"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
 
 Base = declarative_base()
